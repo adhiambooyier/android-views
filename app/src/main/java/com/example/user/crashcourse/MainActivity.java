bce5b95ad4b0fb4da89f1e13f18ff6e66fcbead8
Adhiambo Oyier <violetperez59@yahoo.com>
2018-03-11 12:33:50 +0300

    There is a TextView, ImageView and Button in a LinearLayout, orientation vertical

---
+++ app/src/main/java/com/example/user/crashcourse/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.user.crashcourse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnList = (Button) findViewById(R.id.btnList);
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
