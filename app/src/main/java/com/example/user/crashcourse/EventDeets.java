18cca47916a651713b08ae44be6992d9ea2e0d38
adhiambooyier <violetperez59@yahoo.com>
2018-03-15 01:09:40 +0300

    added a webview. The onclick listener for the ListView Item supposed to load the webview isn't working yet

--- app/src/main/java/com/example/user/crashcourse/EventDeets.java
+++ app/src/main/java/com/example/user/crashcourse/EventDeets.java
@@ -3,11 +3,11 @@ package com.example.user.crashcourse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ListView extends AppCompatActivity {
+public class EventDeets extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_view);
+        setContentView(R.layout.activity_event_deets);
     }
 }
